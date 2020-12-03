ef1fd73bbea12cd8569d396ef0e6dc1ea088731f
Divya <divyav.cs.006@gmail.com>
2020-12-03 21:32:20 +0530

    Student schema updated

--- models/student.js
+++ models/student.js
@@ -77,6 +77,234 @@ const studentSchema = mongoose.Schema({
         }
     }],
 
+    incomefromEstate_Fields_Lands:[{
+        nameofOwner:{
+            type:String,
+            required: true
+        },
+        relationship:{
+            type:String,
+            required: true
+        },
+        location:{
+            type:String,
+            required: true
+        },
+        natureofcultivation:{
+            type:String,
+            required: true
+        },
+        extentofLandAndDetails:{
+            type:String,
+            required: true
+        },
+        annual_income:{
+            type:Number,
+            required: true
+        }
+
+    }],
+
+    incomefromHouses:[{
+        nameofOwner:{
+            type:String,
+            required: true
+        },
+        relationship:{
+            type:String,
+            required: true
+        },
+        assessmentNo:{
+            type:String,
+            required: true
+        },
+        noofHouseholders:{
+            type:String,
+            required: true
+        },
+        adddress:{
+            type:String,
+            required: true
+        },
+        annual_income:{
+            type:Number,
+            required: true
+        }
+
+    }],
+
+    employed:[{
+        establishmentName:{
+            type:String,
+            required: true
+        },
+        establishmentAddress:{
+            type:String,
+            required: true
+        },
+        post:{
+            type:String,
+            required: true
+        },
+        salaryScale:{
+            type:Number,
+            required: true
+        },
+        dateOfAppoinment:{
+            type:Date
+        }
+    }],
+
+    married:[{
+        spouseName:{
+            type:String,
+            required: true
+        },
+        dateOfMarriage:{
+            type:Date,
+            required: true
+        },
+        spousePost:{
+            type:String,
+            required: true
+        },
+        spouseEstablishmentName:{
+            type:String,
+            required: true
+        },
+        spouseMonthlySalary:{
+            type:Number,
+            required: true
+        }
+    }],
+
+    parentsDetails: [{
+        father:[{
+            fatherFullName: {
+                type: String,
+                required: true
+            },
+
+            aliveORnot: {
+                type: String,
+                required: true
+            },
+
+            ageYearMonth: {
+                type: String,
+                required: true
+            },
+
+            fatherOccupation: {
+                type: String,
+                required: true
+            },
+
+            fatherWorkPlace: {
+                type: String,
+                required: true
+            },
+
+            fatherAnnualIncome:{
+                type:Number,
+                required: true
+            },
+            fatherAnnualPropertyIncome:{
+                type:Number,
+                required: true
+            },
+
+            fatherAnnualOtherIncome:{
+                type:Number,
+                required: true
+            },
+
+            fatherTotalAnnualIncome:{
+                type:Number,
+                required: true
+            }
+        }],
+
+        mother:[{
+            motherFullName: {
+                type: String,
+                required: true
+            },
+
+            aliveORnot: {
+                type: String,
+                required: true
+            },
+
+            ageYearMonth: {
+                type: String,
+                required: true
+            },
+
+            motherOccupation: {
+                type: String,
+                required: true
+            },
+
+            motherWorkPlace: {
+                type: String,
+                required: true
+            },
+
+            motherAnnualIncome:{
+                type:Number,
+                required: true
+            },
+            motherAnnualPropertyIncome:{
+                type:Number,
+                required: true
+            },
+
+            motherAnnualOtherIncome:{
+                type:Number,
+                required: true
+            },
+
+            motherTotalAnnualIncome:{
+                type:Number,
+                required: true
+            }
+        }],
+
+        guardian:[{
+            guardianFullName: {
+                type: String,
+                required: true
+            },
+
+            permenentAddress: {
+                type : String,
+                required : true
+            },
+
+            motherOccupation: {
+                type: String,
+                required: true
+            },
+
+            guardianAnnualIncome:{
+                type:Number,
+                required: true
+            },
+            guardianAnnualPropertyIncome:{
+                type:Number,
+                required: true
+            },
+
+            guardianAge: {
+                type: Number,
+                required: true
+            }
+        }]
+    }],
+
+
+
 })
 
 const Student = mongoose.model('student', studentSchema)
