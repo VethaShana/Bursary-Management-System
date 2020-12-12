8cf92332146b0eb3233d9520838e7e99c850021f
unknown <vvvttt479@gmail.com>
2020-12-13 00:01:49 +0530

    userfile modified

--- controllers/user.js
+++ controllers/user.js
@@ -1,43 +1,42 @@
 import User from '../models/user.js'
 import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
-//import auth from '../middleware/auth'
+//import auth from '../middleware/auth.js'
 
-/*
+
 // Register
-router.post("/register", async (req, res) => {
+
+export const Register = async (req, res) => {
     try {
-    let { email, password, passwordCheck, displayName } = req.body;
+    let { email, password, passwordCheck, userName } = req.body;
     // validate
     if (!email || !password || !passwordCheck)
     return res.status(400).json({ msg: "Not all fields have been entered." });
-    if (password.length < 5)
-    return res
-    .status(400)
-    .json({ msg: "The password needs to be at least 5 characters long." });
+
+    if (password.length < 8)
+    return res.status(400).json({ msg: "The password needs to be at least 5 characters long." });
+
     if (password !== passwordCheck)
-    return res
-    .status(400)
-    .json({ msg: "Enter the same password twice for verification." });
+    return res.status(400).json({ msg: "Enter the same password twice for verification." });
+
     const existingUser = await User.findOne({ email: email });
     if (existingUser)
-    return res
-    .status(400)
-    .json({ msg: "An account with this email already exists." });
-    if (!displayName) displayName = email;
+    return res.status(400).json({ msg: "An account with this email already exists." });
+
+    if (!userName) userName = email;
     const salt = await bcrypt.genSalt();
     const passwordHash = await bcrypt.hash(password, salt);
     const newUser = new User({
     email,
     password: passwordHash,
-    displayName,
+    userName,
     });
     const savedUser = await newUser.save();
     res.json(savedUser);
     } catch (err) {
     res.status(500).json({ error: err.message });
     }
-    });*/
+    };
 
 
     // Login
@@ -54,13 +53,14 @@ router.post("/register", async (req, res) => {
             // validate
             if (!email || !password)
                 return res.status(400).json({ msg: "Not all fields have been entered." });
+
             const user = await User.findOne({ email: email });
             if (!user)
-                return res
-                .status(400)
-                .json({ msg: "No account with this email has been registered." });
+                return res.status(400).json({ msg: "No account with this email has been registered." });
+
             const isMatch = await bcrypt.compare(password, user.password);
             if (!isMatch) return res.status(400).json({ msg: "Invalid credentials." });
+            
             const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
                 res.json({
                 token,
@@ -77,20 +77,22 @@ router.post("/register", async (req, res) => {
 
     }
 
-    /*
+    
     // Delete
-    router.delete("/delete", auth, async (req, res) => {
+    export const Delete = async (req, res) => {
+    
     try {
     const deletedUser = await User.findByIdAndDelete(req.user);
     res.json(deletedUser);
     } catch (err) {
     res.status(500).json({ error: err.message });
     }
-    });
+    };
    
 
     // Check if token is valid
-    router.post("/tokenIsValid", async (req, res) => {
+    
+    export const tokenIsValid = async (req, res) => {
     try {
     const token = req.header("x-auth-token");
     if (!token) return res.json(false);
@@ -102,11 +104,12 @@ router.post("/register", async (req, res) => {
     } catch (err) {
     res.status(500).json({ error: err.message });
     }
-    });
-    router.get("/", auth, async (req, res) => {
+    };
+    
+    export const getUser = async (req, res) => {
     const user = await User.findById(req.user);
     res.json({
-    displayName: user.displayName,
+    userName: user.userName,
     id: user._id,
     });
-    });*/
+    };
