c6b8b3f2d7bd8aa972d51b58669d9dca6f76f5b6
unknown <vvvttt479@gmail.com>
2021-02-06 14:38:25 +0530

    GS update

--- client/src/utils/data.js
+++ client/src/utils/data.js
@@ -30,6 +30,400 @@ export const GSDivisions = [
 	{
 		division: 'Pahathadumbara',
 		divisionNo: '#GS10119',
+
+		/*
+		//Batticaloa
+//Manmunai north
+
+Amirthakali 173
+Arasady 177A
+Barathypuram 173H
+Datch Bar 172C
+Gnasooriyam Squre 173B
+Iruthayapuram Central 176H
+Iruthayapuram East 173B
+Iruthayapuram West 176E
+Jayanthipuram 177G
+Kallady 171
+Kallady Mugathuvaram 172A
+Kallady Uppodai 171B
+Kallady Veloor 171A
+Karuweppankeney 176B
+Koddamunai 178
+Kokkuvil 176
+Koolavadi East 173I
+Koolavady 173F
+Mamagam 173A
+Manjanthoduvai North 168
+Manjanthoduvai South 168A
+Navalady 172
+Navatkudah 168B
+Navatkudah East 168C
+Navatkudah South 168D
+Nochchimunai 171C
+Palameenmadu 173C
+Panichchayadi 176D
+Periya Uppodai 178A
+Periya Urani 176C
+Puliyanthivu Central 179B
+Puliyanthivu East 179A
+Puliyanthivu South 179C
+Puliyanthivu West 179
+Punnachcholai 173D
+Puthunagar 181C
+Saththurukondan 176A
+Sethukudah 181D
+Sinna Urane 176F
+Thamaraikerny 177B
+Thandavanvely 177
+Thimilathivu 181
+Thiraimadu 173E
+Thiruchchenthur 172B
+Thirupperunthurai 181A
+Thissaveerasingam Squre 173J
+Veddukkadu 173K
+Veechikalmunai 181B
+
+
+//Koralai pattu North
+
+
+Ammanthanavely	213D
+Kaddumurivu	213B
+Kathiravely	213
+Kayankerny	211A
+Kirmichai	211F
+Mankerny Central	211
+Mankerny South	211G
+Mathurankernikulam	211E
+Palchenai	213A
+Panichchankerni	211C
+Puchchakerny	213C
+Punanai East	211B
+Uriyankaddu	212B
+Vakarai Central	212
+Vakarai North	212A
+Vattavan	211D
+
+//Koralai Pattu West(Oddamavadi)
+
+Mancholai	207A
+Meeravodai  Muslim  East	207B
+Meeravodai Muslim West	207
+Oddamavadi  02	208C
+Oddamavadi 01 North	208B/2
+Oddamavadi 01 South	208B
+Oddamavadi 03	208
+Paper Town	210B
+
+//Koralai pattu (Valaichenai)
+
+Kalkuda	204
+Kalmadu	204A
+Kannagipuram	205D
+Kanniyadi    #	202B
+Karuwakerni	202A
+Kumburumoolai	203B
+Meeravodai Tamil	202
+Nasivanthivu 205C
+Pethalai	205B
+Puthukudiyiruppu	205A
+Sungankerny	202C
+Valaichenai Tamil	205
+
+//Eravur pattu
+
+Arumugathan Kudiyiruppu 2	189C
+ArumugathanKudiruppu 1	189
+Chenkalady 1	195/1
+Chenkalady 2	195
+Ellainagar	174
+Eralakkulam	201A
+Eravur 4	194
+Eravur 5	194/1
+Gamunupura	135A
+Iyankerny	193C
+Kaluvankerny I	198
+Kaluwankerny 2	198A
+Karadiyanaru	185A
+Kithulwewa	185B
+Koduwamadu	195A
+Kommathurai East	196
+Kommathurai North	196B
+Kommathurai West	196A
+Koppavely	146A
+Kumaraveliar Kiramam	195B
+Marappalam	185C
+Mavadivembu 2	197E
+Mavadivembu I	197A
+Mayilavaddavan	197D
+Mylampavely	189B
+Palachcholai	197C
+Pankudahvely	186
+Periya pullumalai	146
+Savukkady	193A
+Siththandi  I	201/1
+Siththandi 2	201/02
+Siththandi 3	201/3
+Siththandi 4	201
+Thalavai	193
+Thannamunai	189A
+Urugamam	185
+Vantharumoolai East	197
+Vantharumoolai West	197B
+Veppavedduwan	186A
+
+
+// Eravur Town
+
+
+Eravur 01	190
+Eravur 01A	190A
+Eravur 01B	190B
+Eravur 02	191
+Eravur 02B	191B
+Eravur 02C	191C
+Eravur 03	192
+Eravur 03A	192B
+Eravur 06	192A
+Eravur 06A	192C
+Eravur 06D	192D
+
+
+
+//Manmunai west
+
+
+Ayithiyamalai North	184B
+Ayithiyamalai South	184C
+Eachchantheivu	182
+Ilupadichenai	148
+Kanchirankudah	148D
+Kannankudah	149
+Kanthinagar	148C
+Karaveddy	183
+Karayakkanteivu	149A
+Kothiyapulai	148E
+Kurinchamunai	149B
+Mahilavedduvan	184
+Mandapathady	149C
+Mankikaddu	182C
+Narippulthoddam	184D
+Navatkadu	182B
+Nediyamadu	184E
+Panchenai	148B
+Paruthichenai	149D
+Pavakkodichenai	148A
+Puthumandapathady	149E
+Unnichchei	184A
+Vavunatheivu	182A
+Vilavedduwan	183A
+	
+	
+//Kattankudy
+	
+	
+	
+165 A Div.3 West Kattankudy	165A
+Div.05, South Kattankudy	164B
+Division 04, Kattankudy	164
+Kattankudy 1 south	167E
+Kattankudy 2	166
+Kattankudy 2 North	166A
+Kattankudy 3	165
+Kattankudy 3 East	165B
+Kattankudy 4 West	164C
+Kattankudy 5	164A
+Kattankudy 6 south	162
+Kattankudy 6 west	162B
+Kattankudy Division 1	167
+Kattankudy Division 6 South	162A
+New Kattankudy Div. North	167A
+New Kattankudy East	167B
+New Kattankudy South	167C
+New Kattankudy West	167D
+
+//Manmunai pattu(Araiyampathy)
+
+
+Araipattay East	157C
+Araipatti 02	157
+Araipatti 03	156
+Araipatti Central	156D
+Araiyampathy South	156B
+Arayampathy 01	158
+Arayampathy North	157D
+Arayampathy West	158A
+Kankeyanodai	155
+Kankeyanodai South	155B
+Kirankulam	150B
+Kirankulam Central	150C
+Kirankulam North	150
+Kirankulam South	150A
+Kovilkulam	156C
+Manmunai	153A
+Mavilankanthurai	154
+Ollikulam	155A
+Palamunai	152
+Puthukudiyiruppu	151
+Puthukudiyiruppu North	151B
+Puthukudiyiruppu South	151A
+Rajathurai Gramam	156A
+Selvanagar East	157A
+Selvanagar East	157B
+Thalankudah	153
+Vedarkudiyiruppu	153B
+
+
+//Manmunai south-west
+
+Ampilanthurai	127
+Ampilanthurai North	127B
+Ampilanthurai West	127A
+Arasaditivu	128
+Arasaditivu North	128C
+Kachchakodi	135D    C
+Kadukkamunai	128A
+Katchenai	127C
+Kokkadichcholai	134
+Kokkadichcholai South	134A
+Kuluvinamadu	135B
+Mahiladithivu	129B
+Mahiladithivu South	129A
+Mavadimunmaari	135
+Munaikadu East	133
+Munaikkadu North	133C
+Munaikkadu South	133B
+Munaikkadu West Part	133A
+Muthalaikkudah	131
+Muthalaikkudah West	131A
+Paddippalai	128B
+Pandariyavelly	129
+Panichchiyadimunmari	135A      D
+Thanthamalai	135C    A 
+
+// porativu pattu
+
+
+
+Anaikaddiyaveli	99E
+Ganeshapuram	98C
+Kakkachchivattai	99A
+Kalumunthanvely	107E
+Kannapuram	99G
+Kannapuram East	99K
+Kanthipuram	107B
+Kovilporativu	109
+Kovilporativu South	109A
+Kovilporativu West	109B
+Malayarkaddu	99H
+Mandur 1 and 2 South	97B
+Mandur 1, 2 (North Part)	97
+Mandur 3	96
+Mandur Kodamunai	96A
+Mavetkudah	107D
+Munaiththeevu	108A
+Navagirinagar	107A
+Nellikkadu	99F
+Palacholai	99I
+Palamunai	97A
+Palayadivaddai	99B
+Palugamam 02	106
+Paluhamam I	107
+Pattapuram	108C
+Periyaporativu	108
+Punnakulam	108B
+Ranamadu	98A
+Sankarpuram	98B
+Saravanaiyadiyoottu	106E
+Selvapuram	107F
+Sinnawaththai	99C
+Thambalawaththai	98
+Thikkodai	106A
+Thumbankerny	106C
+Thumpankerny (Y.F.S.)	107H
+Vammiyadiyoottu	107C
+Vanninakar	106D
+Veeranchenai	106B
+Vellaveli	99
+Vilanthoddam	99D
+Vipulananthapuram	107G
+Vivekananthapuram	99J
+
+
+//Manmunai south and eruvil pattu
+
+ Cheddipalayam North	126A 1
+Cheddipalayam South	126A
+Eruvil East	115B
+Eruvil North	115
+Eruvil South	115A
+Kaluthavalai  01	119
+Kaluthavalai 02	119A
+Kaluthavalai 03	117
+Kaluthavalai 04	117A
+Kaluthavalai 04 Santhipuram	117B
+Kaluthavalai Central	119B
+Kaluwanchikudy - South	116A
+Kaluwanchikudy North	116B
+Kaluwanchikudy North 1	116
+Koddaikallar  South	113B
+Koddaikallar East	113A
+Koddaikallar North	113
+Koddaikallar West	113C
+Kurukkalmadam North	126C
+Kurukkalmadam South	126
+Kurumanvelly 11 West	114B
+Kurumanvely - 12	114A
+Kurumanvely 11 East	114
+Mahiloor East	110A
+Mahiloor South West	110C
+Mahiloor West	110
+Mahiloormunai	110B
+Mangkaadu	123
+Ondachchimadam North	111
+Ondachchimadam South	111A
+Paddiruppu	116C
+Periyakallar 01	76
+Periyakallar 2 West	76B
+Periyakallar 3	75B
+Periyakallar 3 South	75C
+Periyakallaru 01 South	76A
+Periyakallaru 2	75A
+Thethatheevu South - 02	122A
+Thettaththatheevu North	122
+Thettaththeevu South	122C
+Thetthaththeevu South - 01	122B
+Thuraineelavanai North	74
+Thuraineelavanai North - 01	74B
+Thuraineelavanai South	74A
+Thuraineelavanai South - 01	74C
+
+
+//Koralai pattu South(Kiran)
+
+Devapuram	199A #
+Kallichchai	210A #
+Kiran East	203 #
+Kiran West	203A #
+Korakallimadu	203C #
+Koraveli	209B #
+Kudumpimalai	209D #
+Murakkottanchenai	199 #
+Muruththanai	209A #
+Palaiyadithona	200B #
+Perillaveli	209 #
+Poolakkadu	209C #
+Punanai West	210E #
+Santhiveli	200 #
+Thihikilivaddai	200A #
+Uthuchchenai	210A/II #
+Vadamunai	210A/1 #
+Vahaneri	210 #
+
+//Koralai pattu central
+
+*/
 	},
 ]
 
--- routes/students.js
+++ routes/students.js
@@ -1,16 +1,16 @@
 import express from 'express'
 import auth from '../middleware/auth.js'
-import { getStudents, createStudent,deleteStudent,updateStudent,PDF } from '../controllers/students.js'
+import { getStudents, createStudent,deleteStudent,updateStudent,PDFStudent,PDF } from '../controllers/students.js'
 //import auth2 from '../middleware/auth.js'
 
 const router = express.Router()
 
 // protected
 router.get('/', auth, getStudents)
-router.post('/', createStudent)
+router.post('/', createStudent,PDF)
 router.delete('/:id',auth, deleteStudent)
 router.put('/:id',auth,updateStudent)
-router.post('/pdf/:id',auth,PDF)
+router.post('/pdf',PDFStudent)
 // update all other routes
 
 export default router
