685c49377af6f340fc5e558f32e575598fa2dc08
unknown <vvvttt479@gmail.com>
2021-05-09 00:47:56 +0530

    add installment model

---
+++ models/installment.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const installmentSchema = mongoose.Schema({
+  //or Faculty
+  stream: {
+    type: String,
+    required: true,
+  },
+  //years of studies
+  year: {
+    type: String,
+    required: true,
+  },
+
+  acadamicYear: {
+    type: String,
+    required: true,
+  },
+  //from which year
+  from: {
+    type: String,
+    required: true,
+  },
+  // to which year
+  to: {
+    type: String,
+    required: true,
+  },
+});
+
+const User = mongoose.model("installment", installmentSchema);
+
+export default Installment;
