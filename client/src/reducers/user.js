b22b4c2c126c35b16d9e1d3782d72fb400792476
zmardil <btzardil@gmail.com>
2021-05-10 03:36:23 +0530

    utils

--- client/src/reducers/user.js
+++ client/src/reducers/user.js
@@ -1,11 +1,16 @@
 import { SET_USER } from '../actions/types'
 
-const initialState = {}
+const initialState = {
+	data: {
+		name: 'Ardil Mohamed',
+	},
+	isLoading: true,
+}
 
 export function user(state = initialState, action) {
 	switch (action.type) {
 		case SET_USER:
-			return action.payload
+			return { ...state, data: action.payload }
 		default:
 			return state
 	}
