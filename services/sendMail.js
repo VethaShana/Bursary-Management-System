fe2eecc0f03113f7d9bdeccad84b9e819b3064c7
unknown <vvvttt479@gmail.com>
2021-05-11 21:33:17 +0530

    Conflict correction

---
+++ services/sendMail.js
@@ -0,0 +1,48 @@
+import nodemailer from "nodemailer";
+import dotenv from "dotenv";
+dotenv.config();
+
+const NODE_ENV = process.env.NODE_ENV;
+
+const SMTP_HOST = process.env.SMTP_EMAIL || "welfare.univ.jfn.ac.lk";
+const SMTP_EMAIL = process.env.SMTP_EMAIL || "johndoe.teamproject@gmail.com";
+const SMTP_PASSWORD = process.env.SMTP_PASSWORD || "Asd123!@#";
+const SMTP_PORT = process.env.SMTP_PORT || 587;
+
+// data = {to, subject, text, html, attachment: pdf object goes here}
+// to=
+// subject =
+// text =
+// attachment =
+
+export default (data) => {
+  let transporterOptions = {
+    service: "gmail",
+    auth: {
+      user: SMTP_EMAIL,
+      pass: SMTP_PASSWORD,
+    },
+  };
+
+  if (NODE_ENV === "production") {
+    transporterOptions = {
+      host: SMTP_HOST,
+      port: SMTP_PORT,
+      auth: {
+        user: SMTP_EMAIL,
+        pass: SMTP_PASSWORD,
+      },
+    };
+  }
+
+  const transporter = nodemailer.createTransport(transporterOptions);
+
+  const mailOptions = {
+    from: SMTP_EMAIL,
+    ...data,
+  };
+
+  transporter.sendMail(mailOptions, (err) =>
+    err ? console.log(err) : console.log(`Mail sent to ${mailOptions.to}`)
+  );
+};
